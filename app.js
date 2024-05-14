46595595796bb10280240676fc81fc2916c47299
ArionDutta33 <ariondutta333@outlook.com>
2024-05-14 20:28:51 +0530

    Completed the basic structure

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const cursor=document.querySelector(".cursor")
 const h1=document.querySelector("h1")
 const webdesign=document.querySelector("#one")
 const cursorImageWebdesign=document.querySelector(".cursor img")
+const overlay=document.querySelector(".cursor .overlay")
+
+
+//first header
+
+
+const webDevelopment=document.querySelector("#two")
  const images={
-    webdesign:"https://images.unsplash.com/photo-1514179974491-a7885781ed87?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bHV4dXJ5fGVufDB8fDB8fHww",
-    developer:"https://images.unsplash.com/photo-1618020405355-435b53dc2cd7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFuY3klMjBzcG9ydHN8ZW58MHx8MHx8fDA%3D"
+    webdesign:"https://images.pexels.com/photos/7120851/pexels-photo-7120851.jpeg?auto=compress&cs=tinysrgb&w=600",
+    developer:"https://images.pexels.com/photos/1524620/pexels-photo-1524620.jpeg?auto=compress&cs=tinysrgb&w=600"
  }
 function loaderAnimation(){
     gsap.to(page1,{
@@ -26,13 +33,44 @@ loaderAnimation()
 main.addEventListener("mousemove",(e)=>{
     if(e.target===webdesign){
         webdesign.addEventListener("mouseenter",()=>{
+            overlay.style.display="block"
              cursor.style.backgroundImage=`url(${images.webdesign})`
+            //  overlay.style.display="block"
+            //  gsap.to(overlay,{
+            //     transform:"translateX(100%)",
+            //     ease:"none",
+            //     repeat:-1,
+            //     delay:2
+            //  })
+            
         })
         webdesign.addEventListener("mouseleave",()=>{
              cursor.style.backgroundImage=""
             //  cursor.style.backgroundColor="red"
+            overlay.style.display="none"
         })
 
+    }else if(e.target===webDevelopment){
+        webDevelopment.addEventListener("mouseenter",()=>{
+
+            overlay.style.display="block"
+            cursor.style.backgroundImage=`url(${images.developer})`
+           //  overlay.style.display="block"
+           //  gsap.to(overlay,{
+           //     transform:"translateX(100%)",
+           //     ease:"none",
+           //     repeat:-1,
+           //     delay:2
+           console.log("mouse enter")
+           //  })
+        //    overlay.style.display="block"
+       })
+       webDevelopment.addEventListener("mouseleave",()=>{
+        cursor.style.backgroundImage=""
+        console.log("mouse leave")
+       //  cursor.style.backgroundColor="red"
+       overlay.style.display="none"
+   })
     }
     // console.log(e)
     // if(e.target===)
